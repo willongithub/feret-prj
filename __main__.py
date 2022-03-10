7020974bf65663a4ef1e721cc2fa3c5cbc82a9ba
liam@macos <me@macos.com>
2022-03-10 23:29:17 +1100

    feret prj main update

---
+++ __main__.py
@@ -0,0 +1,10 @@
+# Project for Assessment
+# Evaluation of face_recognition package via FERET dataset
+# ========================================================
+"""Entry point."""
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
